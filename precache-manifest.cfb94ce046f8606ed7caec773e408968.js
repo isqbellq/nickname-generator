4198493577e536d13b0b3a95d49cc8a6e7e39c07
isqbellq <isabcdef@outlook.it>
2019-02-25 13:18:21 +0100

    Updates

---
+++ precache-manifest.cfb94ce046f8606ed7caec773e408968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd6b59aef280f537c2ce",
+    "url": "/sample-react-app/static/js/runtime~main.dd6b59ae.js"
+  },
+  {
+    "revision": "0b6cd9cbb5e256e94594",
+    "url": "/sample-react-app/static/js/main.0b6cd9cb.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/sample-react-app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0b6cd9cbb5e256e94594",
+    "url": "/sample-react-app/static/css/main.8855960d.chunk.css"
+  },
+  {
+    "revision": "0ae41db13e5f457a71026e6fedf02b32",
+    "url": "/sample-react-app/index.html"
+  }
+];
