da662d6d1c4260bddd521704f19e5b3de4528a9c
isqbellq <isabcdef@outlook.it>
2019-03-07 22:36:47 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import GetActivity from "./components/GetActivity";
-import CurrentDate from "./components/Date";
-import HelloWorld from "./components/HelloWorld";
-import Alert from "./components/Alert";
-import Converter from "./components/Converter";
-import Form from "./components/Form";
 
 class App extends Component {
   render() {
@@ -13,17 +8,9 @@ class App extends Component {
       <div className="App">
           <h1>Time tracker</h1>
           <GetActivity />
-
       </div>
     );
   }
 }
 
 export default App;
-
-
-// <CurrentDate />
-// <HelloWorld />
-// <Alert />
-// <Converter />
-// <Form />
