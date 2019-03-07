1bd610b9395b7a7e2ac8bd93d0f03d8bbb900a5b
isqbellq <isabcdef@outlook.it>
2019-03-07 22:48:04 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import GetActivity from "./components/GetActivity";
 import Activities from "./components/Activities";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: "",
+      end: "",
+      activities: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
           <h1>Time tracker</h1>
-          <GetActivity />
+          <GetActivity 
+          start={this.state.start}
+          end={this.state.end}
+          activities={this.state.activities} 
+          />
           <Activities />
       </div>
     );
--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class Activities extends Component {
 	render() {
-
 		return (
 			<ul className="Activities">
+			    <li>Act 1</li>
+			    <li>Act 2</li>
+			    <li>Act 3</li>
 			</ul>
 		);
 	}
