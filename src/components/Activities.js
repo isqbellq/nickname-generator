0389702479e261780c1e7e4c26f0cab4f65283d8
isqbellq <isabcdef@outlook.it>
2019-03-12 12:08:36 +0100

    updates

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import Activity from "./Activity";
 
 const Activities = props => {
+    const activities = props.activities.map(activity => (
+	    <Activity 
+	        key={activity}
+	        duration={props.duration}
+	        activities={props.activities}
+	    />
+	));   
+
     return (
 		<div className="activities">
-	       {props.activities.map(activity => (
-
-	       	<Activity 
-	       	    duration={props.activity.duration}
-	       	    activities={props.activities}
-	       	    key={activity}
-	       	/>
-	       	))}
+	       {activities}
 		</div>
 	);
 }
