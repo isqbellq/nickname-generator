438016eca8ba5cca3114fbbd90a55d21c9eb1c33
isqbellq <isabcdef@outlook.it>
2019-03-12 18:26:14 +0100

    updates

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 
 const Activity = props => {
 	return (
-		<div className="activity"
-		    key={props.activity}
-		>
+		<div className="activity">
 		    <input type="checkbox" />
             <p>Duration: {props.duration}. Activity: {props.activity}.</p>
         </div>
