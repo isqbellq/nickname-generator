914d2f57bbe8093c586463828f44de9c87994b1a
isqbellq <isabcdef@outlook.it>
2019-03-12 13:15:51 +0100

    updates

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Activity from "./Activity";
 
 const Activities = props => {
+
+	// props.activities.map(...) but actually need to map both activities AND duration
     const activities = props.activities.map(activity => (
 	    <Activity 
 	        key={activity}
 	        duration={props.duration}
-	        activities={props.activities}
+	        activity={activity}
 	    />
 	));   
 
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 const Activity = props => {
 	return (
-		<div className="activity">
+		<div className="activity"
+		    key={props.activity}
+		>
 		    <input type="checkbox" />
-        	<p>Duration: {props.duration}. Activity: {props.activities}.</p>
+            <p>Duration: {props.duration}. Activity: {props.activity}.</p>
         </div>
 	);
 }
